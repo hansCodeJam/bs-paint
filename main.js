f34c19c5f7e7433be1c6918e3db873b743a2f938
Hans Vos <hans.vos@codeimmersives.com>
2019-11-21 11:35:37 -0500

    ereased backticks

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 function eventColor(event) {
     const listArray = document.querySelector('.current-brush').classList;
-    document.querySelector('.current-brush').classList.replace(`${listArray[1]}`, `${event.target.classList[1]}`)
+    document.querySelector('.current-brush').classList.replace(listArray[1], event.target.classList[1])
 }
 function eventColor1(event) {
     const listArray = document.querySelector('.current-brush').classList;
-    event.target.classList.replace(`${event.target.classList[1]}`, `${listArray[1]}`)
+    event.target.classList.replace(event.target.classList[1], listArray[1])
 }
 
 const colorSquare = document.querySelectorAll('.square');
+
 const colorPalette = document.querySelectorAll('.palette-color');
 
 for(let i = 0; i < colorPalette.length; i++) {
